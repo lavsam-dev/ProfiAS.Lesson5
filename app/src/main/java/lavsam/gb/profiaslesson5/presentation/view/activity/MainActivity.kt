f47bd21172dca40864b32c6d05608a21bf7ced66
lavsam-dev <lavsam@gmail.com>
2021-12-23 22:10:48 +0400

    SplashScreen

--- app/src/main/java/lavsam/gb/profiaslesson5/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/lavsam/gb/profiaslesson5/presentation/view/activity/MainActivity.kt
@@ -1,10 +1,18 @@
 package lavsam.gb.profiaslesson5.presentation.view.activity
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.LinearLayoutManager
 import lavsam.gb.profiaslesson5.R
 import lavsam.gb.profiaslesson5.core.presentation.view.activity.base.BaseActivity
@@ -18,9 +26,7 @@ import lavsam.gb.profiaslesson5.presentation.view.fragment.SearchDialogFragment
 import lavsam.gb.profiaslesson5.presentation.viewModel.MainActivityViewModel
 import lavsam.gb.profiaslesson5.utils.convertMeaningsToString
 import org.koin.androidx.viewmodel.ext.android.viewModel
-
-private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG = "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
-private const val VIEWMODEL_INIT_FIRST = "The ViewModel should be initialised first"
+import kotlin.random.Random
 
 class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
@@ -66,6 +72,7 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setDefaultSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -97,7 +104,6 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
         if (binding.mainActivityRecyclerview.adapter != null) {
             throw IllegalStateException(VIEWMODEL_INIT_FIRST)
         }
-//        val mainActivityViewModel: MainActivityViewModel by viewModel()
         val mainActivityViewModel: MainActivityViewModel by viewModel()
         viewModel = mainActivityViewModel
         viewModel.subscribe().observe(this@MainActivity, { renderData(it) })
@@ -110,4 +116,86 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
             mainActivityRecyclerview.adapter = adapter
         }
     }
+
+    private fun setDefaultSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (Random.nextBoolean()) {
+                setSplashScreenAnimation()
+            } else {
+                setSplashScreenHideAnimation()
+            }
+        } else {
+            setSplashScreenDuration()
+        }
+    }
+
+    @RequiresApi(31)
+    private fun setSplashScreenAnimation() {
+        val splashScreen = installSplashScreen()
+        splashScreen.setOnExitAnimationListener { splashScreenProvider ->
+            ObjectAnimator.ofFloat(
+                splashScreenProvider.view,
+                View.TRANSLATION_Y,
+                START_ANIMATION,
+                -splashScreenProvider.view.height.toFloat()
+            ).apply {
+                interpolator = AnticipateInterpolator()
+                duration = SLIDE_UP_DURATION
+                doOnEnd { splashScreenProvider.remove() }
+            }.start()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setSplashScreenHideAnimation() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideLeft = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_X,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideLeft.interpolator = AnticipateInterpolator()
+            slideLeft.duration = SLIDE_LEFT_DURATION
+
+            slideLeft.doOnEnd { splashScreenView.remove() }
+            slideLeft.start()
+        }
+    }
+
+    private fun setSplashScreenDuration() {
+        var isHideSplashScreen = false
+
+        object : CountDownTimer(COUNTDOWN_DURATION, COUNTDOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
+    }
+
+    companion object {
+        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG = "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
+        private const val VIEWMODEL_INIT_FIRST = "The ViewModel should be initialised first"
+        private const val SLIDE_UP_DURATION = 1500L
+        private const val START_ANIMATION = 0f
+        private const val SLIDE_LEFT_DURATION = 1000L
+        private const val COUNTDOWN_DURATION = 2000L
+        private const val COUNTDOWN_INTERVAL = 1000L
+    }
 }
+
